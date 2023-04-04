cfc11df0831dfeac21b80ae3869ef3eaae06e3b8
aguxtest <aguxgarciabackupsoft_0@protonmail.com>
2023-04-03 23:02:57 -0300

    Updates

--- assets/HomeCustom-df256c1a.js
+++
@@ -1,1 +0,0 @@
-import{H as e,a as t,b as s}from"./app-27f9bd6d.js";import m from"./HomeHeroCustom-d1510471.js";import{c as a,p as r,q as n,O as o,_}from"./framework-24a298fe.js";const c={class:"home"},u=a({__name:"HomeCustom",setup(p){return(C,H)=>(r(),n("main",c,[o(m),o(e),o(t),o(s)]))}}),d=_(u,[["__file","HomeCustom.vue"]]);export{d as default};
